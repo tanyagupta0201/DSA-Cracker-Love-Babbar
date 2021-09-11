e9e70b4e1551fcdeb325b8bcccc5571be499ae73
Tanya Gupta <tanyagupta.pg@gmail.com>
2021-09-12 00:02:49 +0530

    Added Detect loop in a Linked List

---
+++ 05-Linked List/03-Detect-Loop-In-Linked-List.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    // Constructor
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+void print(node *head)
+{
+    if (head == NULL)
+    {
+        cout << "The list is empty !!" << endl;
+    }
+
+    while (head != NULL)
+    {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL";
+    cout << endl;
+}
+
+void Insert_At_Tail(node *&head, int data)
+{
+    if (head == NULL)
+    {
+        head = new node(data);
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new node(data);
+    return;
+}
+
+bool detectLoop(node *head)
+{
+    // your code here
+    if (head == NULL)
+    {
+        return false;
+    }
+
+    node *fast = head;
+    node *slow = head;
+
+    while (fast != NULL and fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    node *head = NULL;
+    int n, data;
+    cout << "Enter the number of nodes you want to create: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the data: ";
+        cin >> data;
+        Insert_At_Tail(head, data);
+    }
+
+    cout << "Linked List: ";
+    print(head);
+
+    if (detectLoop(head))
+        cout << "Loop found";
+    else
+        cout << "No Loop";
+
+    return 0;
+}
