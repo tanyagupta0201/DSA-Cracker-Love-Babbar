5ee83433ded88c5e9f18f7a9e7d1d25598e015a9
Tanya Gupta <tanyagupta.pg@gmail.com>
2021-09-21 18:33:13 +0530

    Added delete loop in Linked List

---
+++ 05-Linked List/04-Delete-Loop-In-Linked-List.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    // Constructor
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+void print(node *head)
+{
+    if (head == NULL)
+    {
+        cout << "The list is empty !!" << endl;
+    }
+
+    while (head != NULL)
+    {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL";
+    cout << endl;
+}
+
+void Insert_At_Tail(node *&head, int data)
+{
+    if (head == NULL)
+    {
+        head = new node(data);
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new node(data);
+    return;
+}
+
+void deleteLoop(node *head)
+{
+    node *fast = head;
+    node *slow = head;
+
+    // Boolean to check if there a loop exists in the given Linked List.
+    int flag = 0;
+
+    while (fast != NULL and fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag)
+    {
+        slow = head;
+
+        while (slow->next != fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        fast->next = NULL;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    int n, data;
+    cout << "Enter the number of nodes you want to create: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the data: ";
+        cin >> data;
+        Insert_At_Tail(head, data);
+    }
+
+    // Create a loop for testing
+    head->next->next->next->next->next = head->next->next;
+
+    // Function to delete if a loop is present
+    deleteLoop(head);
+
+    // Print the Linked List after removing the loop
+    print(head);
+
+    return 0;
+}
